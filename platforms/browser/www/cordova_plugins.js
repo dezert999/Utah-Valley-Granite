54e5c99d67c4d01bc3be5e029d9f7da5db9b9a40
dezert99 <bubbamart99@gmail.com>
2017-05-05 22:11:18 -0400

    V1.0

---
+++ platforms/browser/www/cordova_plugins.js
@@ -0,0 +1,27 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/call-number/www/CallNumber.js",
+        "id": "call-number.CallNumber",
+        "pluginId": "call-number",
+        "clobbers": [
+            "call"
+        ]
+    },
+    {
+        "file": "plugins/call-number/www/CallNumber.js",
+        "id": "call-number.CallNumber",
+        "pluginId": "call-number",
+        "clobbers": [
+            "call"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "call-number": "0.0.2"
+}
+// BOTTOM OF METADATA
+});
