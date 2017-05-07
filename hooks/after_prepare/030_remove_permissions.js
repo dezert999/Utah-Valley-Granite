56b3372a2cf604d8e92e2fe9243cf07c58329029
dezert99 <bubbamart99@gmail.com>
2017-05-07 12:38:02 -0400

    s

--- hooks/after_prepare/030_remove_permissions.js
+++ hooks/after_prepare/030_remove_permissions.js
@@ -1,39 +1,21 @@
-  #!/usr/bin/env node
 
-    //
-    // This hook removes specific permissions from the AndroidManifest.xml
-    // The AndroidManifest is re-generated during the prepare stage,
-    // so this must be run on the "after_prepare" hook.
-    //
+    #!/usr/bin/env node
+var fs = require('fs');
 
+if(fs.existsSync('platforms/android')) {
+    const PERMISSIONS_TO_REMOVE = ["RECORD_AUDIO", "MODIFY_AUDIO_SETTINGS", "READ_PHONE_STATE" ,"READ_CONTACTS","GET_ACCOUNTS"],
+        MANIFEST = 'platforms/android/AndroidManifest.xml';
 
-    // Configure the permissions to be forcefully removed.
-    // NOTE: These permissions will be removed regardless of how many plugins
-    //       require the permission. You can check the permission is only required
-    //       by the plugin you *think* needs it, by looking at the "count" shown in
-    //       your /plugins/android.json file.
-    //       If the count is more than 1, you should search through
-    //       the /plugins//plugin.xml files for <uses-permission> tags.
+        manifestLines = fs.readFileSync(MANIFEST).toString().split('\n'),
+        newManifestLines = [];
 
-    var permissionsToRemove = [ "RECORD_AUDIO", "MODIFY_AUDIO_SETTINGS", "READ_PHONE_STATE" ,"READ_CONTACTS","GET_ACCOUNTS"];
+    const permissions_regex = PERMISSIONS_TO_REMOVE.join('|');
 
+    manifestLines.forEach(function(line) {
+        if(!line.match(permissions_regex)) {
+            newManifestLines.push(line);
+        }
+    });
 
-    var fs = require('fs');
-    var path = require('path');
-    var rootdir = process.argv[2];
-    var manifestFile = path.join(rootdir, "platforms/android/AndroidManifest.xml");
-
-    fs.readFile( manifestFile, "utf8", function( err, data )
-    {
-        if (err)
-            return console.log( err );
-
-        var result = data;
-        for (var i=0; i", "" );
-
-        fs.writeFile( manifestFile, result, "utf8", function( err )
-        {
-            if (err)
-                return console.log( err );
-        } );
-    } );
+    fs.writeFileSync(MANIFEST, newManifestLines.join('\n'));
+}
